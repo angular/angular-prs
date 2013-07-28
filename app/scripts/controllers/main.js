8bbbafdd7ff50df1238fa064a082b20bf1787de9
Igor Minar <igor@angularjs.org>
2013-07-27 22:44:24 -0700

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngPullDashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

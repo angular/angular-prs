a252423bf6e151f203206a4de5d31fe9ae6d72df
Igor Minar <igor@angularjs.org>
2013-07-28 02:35:29 -0700

    github.js - add github service

---
+++ app/scripts/services/github.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('ngPullDashboardApp')
+  .service('github', function Github($http, $q) {
+
+    this.openPrs = function() {
+      return fetchPages('open', 2);
+    };
+
+
+    this.closedPrs = function() {
+      return fetchPages('closed', 50);
+    };
+
+
+    this.prs = function() {
+      return $q.all([this.openPrs(), this.closedPrs()]).then(function(results) {
+        var pulls = [];
+
+        results.forEach(function(result) {
+          pulls = pulls.concat(result);
+        });
+
+        return pulls;
+      });
+    };
+
+
+    function fetchPages(state, lastPage) {
+      var responsePromises = [];
+      var prs = [];
+
+      for (var i = 1; i <= lastPage; i++) {
+        responsePromises.push($http.get('/data/pulls-' + state + '/pulls-page-' + i + '.json'));
+      }
+
+      return $q.all(responsePromises).then(function (responses) {
+        responses.forEach(function(response) {
+          prs = prs.concat(response.data);
+        });
+        return prs;
+      });
+    }
+  });

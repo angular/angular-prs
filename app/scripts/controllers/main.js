90b512030952989f4153a1d80b969d4987fc790e
Igor Minar <igor@angularjs.org>
2013-07-29 01:31:20 -0700

    add week based time series

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('ngPullDashboardApp')
-  .controller('MainCtrl', function ($scope, github) {
+  .controller('MainCtrl', function ($scope, github, dateFilter) {
       var dates = {},
-          timeSeries = [],
+          dayTimeSeries = [],
+          weekTimeSeries = [],
           processedPrs = {},
-          previousDateInfo = {openCount: 0};
+          previousDateInfo = {pendingCount: 0};
 
       console.log('loading data...');
 
@@ -33,22 +34,67 @@ angular.module('ngPullDashboardApp')
         });
 
 
-        console.log('creating time series...');
-
+        console.log('creating day time series...');
 
         Object.keys(dates).sort().forEach(function(date) {
           var dateInfo = dates[date];
           dateInfo.date = date;
           dateInfo.createdCount = dateInfo.createdCount || 0;
           dateInfo.closedCount = dateInfo.closedCount || 0;
-          dateInfo.openCount = previousDateInfo.openCount + dateInfo.createdCount - dateInfo.closedCount;
-          timeSeries.push(dateInfo);
+          dateInfo.pendingCount = previousDateInfo.pendingCount + dateInfo.createdCount - dateInfo.closedCount;
+          dayTimeSeries.push(dateInfo);
 
           previousDateInfo = dateInfo;
         });
 
+
+        console.log('creating week time series...');
+
+        var weeks = {};
+        var weekStart;
+        var weekEndTime = 0;
+        var weekInfo;
+        var previousWeekInfo = {pendingCount: 0};
+        var timeZoneOffset = new Date().getTimezoneOffset()*60*1000;
+
+        Object.keys(dates).sort().forEach(function(date) {
+          var dateInfo = dates[date];
+
+          if (new Date(date).getTime() <= weekEndTime) {
+            weekInfo.createdCount += dateInfo.createdCount;
+            weekInfo.closedCount += dateInfo.closedCount;
+          } else {
+            if (weekStart) {
+              weekStart = dateFilter(weekEndTime + 1 + timeZoneOffset, "yyyy-MM-dd");
+            } else {
+              weekStart = date;
+            }
+            weekEndTime = new Date(weekStart).getTime() + 7*24*60*60*1000;
+
+            weekInfo = {
+              createdCount: dateInfo.createdCount,
+              closedCount: dateInfo.closedCount
+            };
+            weeks[weekStart] = weekInfo;
+          }
+        });
+
+
+        Object.keys(weeks).sort().forEach(function(weekStart) {
+          var weekInfo = weeks[weekStart];
+          weekInfo.date = weekStart;
+          weekInfo.createdCount = weekInfo.createdCount || 0;
+          weekInfo.closedCount = weekInfo.closedCount || 0;
+          weekInfo.pendingCount = previousWeekInfo.pendingCount + weekInfo.createdCount - weekInfo.closedCount;
+          weekTimeSeries.push(weekInfo);
+
+          previousWeekInfo = weekInfo;
+        });
+
+
         console.log('rendering...');
 
-        $scope.timeSeries = timeSeries;
+        $scope.dayTimeSeries = dayTimeSeries;
+        $scope.weekTimeSeries = weekTimeSeries;
       });
   });

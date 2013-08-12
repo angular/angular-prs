b206e16205b5ae43c92e113b91791235a67e1e7f
Igor Minar <igor@angularjs.org>
2013-08-12 14:58:09 -0700

    update

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for i in {1..2}; do curl -vo pulls-open/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=open\&page=$i; done
-for i in {1..50}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i; done
+for i in {1..54}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i; done

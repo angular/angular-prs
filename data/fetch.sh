b2a534e6559d4d9129346f6b80aec3abb6618fd2
Igor Minar <igor@angularjs.org>
2013-11-25 15:31:51 -0800

    updating gh-pages to 24f6519

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for i in {1..3}; do curl -vo pulls-open/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=open\&page=$i; done
-for i in {1..56}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i; done
+for i in {1..7}; do curl -vo pulls-open/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=open\&page=$i; done
+for i in {1..75}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i; done

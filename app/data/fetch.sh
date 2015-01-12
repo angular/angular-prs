e6b6f03aa93e2364421add485d3059ab39ec4fc5
Peter Bacon Darwin <pete@bacondarwin.com>
2015-01-12 13:19:34 +0000

    chore(fetch.sh): add client_id and client_secret arguments to utility
    The GitHub API will block too many anonymous accesses so we must authenticate
    when fetching the PR info.

--- app/data/fetch.sh
+++ app/data/fetch.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-for i in {1..7}; do curl -vo pulls-open/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=open\&page=$i; done
-for i in {1..75}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i; done
+if [ $# -lt 2 ]
+  then
+    echo "Please supply a GitHub client_id and client_secret as arguments"
+    echo "For example: ./fetch.sh 12345678 1a2b3c4d5e6f"
+    exit 1
+fi
+
+client_id=$1
+client_secret=$2
+
+for i in {1..7}; do curl -vo pulls-open/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=open\&page=$i\&client_id=$client_id\&client_secret=$client_secret; done
+for i in {1..75}; do curl -vo pulls-closed/pulls-page-$i.json https://api.github.com/repositories/460078/pulls?state=closed\&page=$i\&client_id=$client_id\&client_secret=$client_secret; done
